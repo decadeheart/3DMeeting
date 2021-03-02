b72c63376194920ff40344a44cd98d4c6ab90e6f
decadeheart <2456535090@qq.com>
2021-03-02 20:30:00 +0800

    添加图片传输服务端

---
+++ imageServer/index.py
@@ -0,0 +1,22 @@
+import eventlet
+import socketio
+
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+@sio.on('connect')
+def connect(sid, env):
+    print('已经连接成功了 ', sid)
+
+@sio.on('img')
+def img(sid,data):
+    print('图片传输中，用户id和数据：',sid,data)
+
+eventlet.wsgi.server(
+    eventlet.wrap_ssl(
+        eventlet.listen(('10.135.22.177', 3000)),
+        certfile='cert.pem',
+        keyfile='key.pem',
+        server_side=True
+    ), 
+    app)
